245b23e710cfadd86681c9ac1fbce7e452e0f39c
KristinaMacias <maciaskristina@gmail.com>
2022-05-08 23:09:07 -0700

    added about.jsx to house about section for website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navabar';
+import About from './components/about';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <About />
       
     </div>
   );
