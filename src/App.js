7b27d54e9326367f18ac310b4158b5fa41d0faf1
KristinaMacias <maciaskristina@gmail.com>
2022-05-08 21:16:18 -0700

    added bootstrap, created a navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navabar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       
     </div>
   );
---
+++ src/components/navabar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default class Navbar extends React.Component {
+    render() {
+        return (
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3 lead">
+                <a class="navbar-brand" href="#">The Groove Collective</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Featured Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Reviews</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
